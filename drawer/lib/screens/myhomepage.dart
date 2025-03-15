0784bd79a76211db521cfadc1c1a41d427bc3da1
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-03-15 22:57:22 +0100

    Add files via upload

---
+++ drawer/lib/screens/myhomepage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:proba2/screens/screen1.dart';
+import 'package:proba2/screens/screen2.dart';
+void navigateToScreen(BuildContext context, Widget screen) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => screen),
+  );
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Whale', style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'This is Sperm Whale'
+            ),
+            Image.network('https://www.anywhere.com/img-a/flora-fauna/sea-mammal/cachalote/sperm-whale.jpg?q=95&w=650'),
+            Text('This is Sperm Whale')
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(child: Text('Title'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () => navigateToScreen(context, MyHomePage(title: 'Home')),
+            ),
+            ListTile(
+              title: const Text('Screen 1'),
+              onTap: () => navigateToScreen(context, Screen1(title: 'Screen 1')),
+            ),
+            ListTile(
+              title: const Text('Screen 2'),
+              onTap: () => navigateToScreen(context, Screen2(title: 'Screen 2')),
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
